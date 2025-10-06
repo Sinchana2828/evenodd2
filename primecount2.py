324e249a398029a7995e4acfc1f53f2d7a432d74
Sinchana2828 <sinchanaballary@gmail.com>
2025-10-06 16:53:28 +0530

    Create primecount2.py

---
+++ primecount2.py
@@ -0,0 +1,17 @@
+def count_even_odd(numbers):
+    even_count = 0
+    odd_count = 0
+
+    for n in numbers:
+        if n % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+
+    print("Even numbers:", even_count)
+    print("Odd numbers:", odd_count)
+
+
+
+
+count_even_odd(numbers)
